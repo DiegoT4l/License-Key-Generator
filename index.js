978c89037ad2909bfbacc78fd1f476a8b6f4db45
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2023-10-14 23:21:16 -0600

    configuracion webpack

---
+++ index.js
@@ -0,0 +1,3 @@
+import './app.js';
+import './node_modules/eins-modal/dist/js/eins-modal.min.js';
+import './css/index.css';
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ['./index.js'],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: false
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[hash].css" }),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        new HtmlWebpackPlugin({ template: './index.html' })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    },
+
+}
