227530212f886922e3fb600d8e796918dfa149bf
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2023-10-27 16:56:54 -0600

    feat(app): Se agregó debouncing patron y comentarios al codigo.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+/**
+ * Generates a random key with the specified length and character types.
+ * @param {number} length - The length of the key.
+ * @param {boolean} includeUppercase - If it should include uppercase letters.
+ * @param {boolean} includeLowercase - If it should include lowercase letters.
+ * @param {boolean} includeNumbers - If it should include numbers.
+ * @returns {string} The generated random key.
+ * @throws {Error} If no character type is selected.
+ */
+// Generates a random key of the specified length, including the specified types of characters.
 function generateRandomKey(length, includeUppercase, includeLowercase, includeNumbers) {
+    // Create a string containing the characters to use when generating the key.
     const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const lowercase = 'abcdefghijklmnopqrstuvwxyz';
     const numbers = '0123456789';
@@ -8,11 +19,7 @@ function generateRandomKey(length, includeUppercase, includeLowercase, includeNu
     if (includeLowercase) characters += lowercase;
     if (includeNumbers) characters += numbers;
 
-    if (characters === '') {
-        console.error('Debe seleccionar al menos un tipo de caracter.');
-        return '';
-    }
-
+    // Generate the key.
     let key = '';
     for (let i = 0; i < length; i++) {
         const index = Math.floor(Math.random() * characters.length);
@@ -22,9 +29,24 @@ function generateRandomKey(length, includeUppercase, includeLowercase, includeNu
     return key;
 }
 
-function validarFormulario(numKeys, charKeys) {
+/**
+ * Valida el formulario y habilita o deshabilita el botón de generación de claves en consecuencia.
+ * @returns {boolean} Verdadero si el formulario es válido, falso de lo contrario.
+ */
 
-    if (charKeys < 6 || charKeys > 20) {
+/*This code validates the form to ensure that the user has entered valid values for the number of
+keys and the length of the keys. It also ensures that the length of the keys is between 6 and 20
+and that the number of keys is between 1 and 10.*/
+function validarFormulario() {
+    const numKeys = parseInt(document.getElementById('numKeys').value, 10);
+    const length = parseInt(document.getElementById('charKeys').value, 10);
+
+    if (isNaN(length) || isNaN(numKeys)) {
+        alert('Debes introducir valores válidos.');
+        disableGenerateButton();
+        return false;
+    }
+    if (length < 6 || length > 20) {
         alert("El número de caracteres debe ser mayor o igual a 6 y menor que 20.");
         disableGenerateButton();
         return false;
@@ -39,21 +61,28 @@ function validarFormulario(numKeys, charKeys) {
     return true;
 }
 
+// This code disables the generate button so that the user cannot click it.
 function disableGenerateButton() {
     const generateButton = document.querySelector('.generate-button');
-    generateButton.disabled = true;
-    console.log('disabled button');
+    if (generateButton) {
+        generateButton.disabled = true;
+        console.log('disabled button');
+    } else {
+        console.error('generateButton not found');
+    }
 }
 
 function enableGenerateButton() {
     const generateButton = document.querySelector('.generate-button');
-    generateButton.disabled = false;
-    console.log('enabled button');
+    if (generateButton) {
+        generateButton.disabled = false;
+        console.log('enabled button');
+    } else {
+        console.error('generateButton not found');
+    }
 }
 
-function generateKeys() {
-    const numKeys = parseInt(document.getElementById('numKeys').value, 10);
-    const length = parseInt(document.getElementById('charKeys').value, 10);
+function generateKeys() { //Genera las llaves
     const includeUppercase = document.getElementById('includeUppercase').checked;
     const includeLowercase = document.getElementById('includeLowercase').checked;
     const includeNumbers = document.getElementById('includeNumbers').checked;
@@ -68,6 +97,8 @@ function generateKeys() {
         return;
     }
 
+    const numKeys = parseInt(document.getElementById('numKeys').value, 10);
+    const length = parseInt(document.getElementById('charKeys').value, 10);
     const keys = [];
     for (let i = 0; i < numKeys; i++) {
         const key = generateRandomKey(length, includeUppercase, includeLowercase, includeNumbers);
@@ -82,11 +113,11 @@ function generateKeys() {
 }
 
 
-function displayKeys(keys) {
+function displayKeys(keys) { //Muestra las llaves
     const keysTable = document.getElementById('keysTable');
     keysTable.innerHTML = '';
 
-    if (keys.length > 0) {
+    if (keys.length > 0) { //Si hay llaves, se muestran en una tabla
         const table = document.createElement('table');
         table.style.margin = 'auto';
 
@@ -108,7 +139,7 @@ function displayKeys(keys) {
             row.appendChild(cell1);
             row.appendChild(cell2);
             table.appendChild(row);
-        }
+        } //Se recorre el array de llaves y se muestran en la tabla
 
         keysTable.appendChild(table);
     }
@@ -118,18 +149,18 @@ function displayKeys(keys) {
     }
 }
 
-function downloadKeys() {
+function downloadKeys() { //Descarga las llaves
     const keys = [];
     const tableRows = document.querySelectorAll('#keysTable table tr');
 
-    tableRows.forEach(row => {
+    tableRows.forEach(row => { //Se recorre la tabla y se obtienen las llaves
         const key = row.cells[1].textContent;
         if (key !== 'License Key') {
             keys.push(key);
         }
     });
 
-    if (keys.length > 0) {
+    if (keys.length > 0) { //Se crea un archivo con las llaves
         const formattedKeys = keys.join('\n');
         const blob = new Blob([formattedKeys], { type: 'text/plain' });
         const a = document.createElement('a');
@@ -142,6 +173,9 @@ function downloadKeys() {
         alert('No hay llaves para descargar.');
     }
 }
+
+
+// IIFE to initialize the app.
 
 (() => {  //footer current Year
     const currentYearElement = document.getElementById("currentYear");
@@ -156,22 +190,34 @@ function downloadKeys() {
     currentYearFooter(currentYearElement);
 })();
 
-(() => {
+(() => { //Generar llaves
     const generateKeysButton = document.querySelector('.generate-button');
     generateKeysButton.addEventListener('click', () => {
         generateKeys();
     });
 })();
 
-(() => {
+(() => { //Descargar llaves
     const generateKeysButton = document.querySelector('.download-button');
     generateKeysButton.addEventListener('click', () => downloadKeys());
-})();
+})(); 
 
-(() => {
-    var numKeys = document.getElementById("numKeys");
-    var charKeys = document.getElementById("charKeys");
+(() => { //Validar formulario
+    const numKeys = document.getElementById("numKeys");
+    const charKeys = document.getElementById("charKeys");
+    let timeout;
 
-    numKeys.addEventListener('input', () => validarFormulario(numKeys.value, charKeys.value));
-    charKeys.addEventListener('input', () => validarFormulario(numKeys.value, charKeys.value));
+    function debounce(func, delay) {
+        clearTimeout(timeout);
+        timeout = setTimeout(func, delay);
+    }
+
+    function onInput() {
+        debounce(() => {
+            validarFormulario();
+        }, 300);
+    }
+
+    numKeys.addEventListener('input', onInput);
+    charKeys.addEventListener('input', onInput);
 })();
